e4bd6f58b92f1db0063cf8c1b976c73a326078e8
yanfuyou <1839224665@qq.com>
2022-12-22 02:25:19 +0800

    文章收藏

--- src/main/java/com/fuyou/community/collect/controller/CollectController.java
+++ src/main/java/com/fuyou/community/collect/controller/CollectController.java
@@ -1,6 +1,8 @@
 package com.fuyou.community.collect.controller;
 
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.fuyou.community.collect.model.Collect;
 import com.fuyou.community.collect.service.CollectService;
@@ -11,6 +13,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
+
 @RestController
 @RequestMapping("/collect")
 @RequiredArgsConstructor
@@ -20,9 +24,12 @@ public class CollectController {
 
     @PostMapping("/save")
     @ApiOperation("添加收藏")
-    public void save(@RequestBody Collect collect) {
-        collect.setId(IdUtil.simpleUUID());
-        collectService.save(collect);
+    public ResultVo<Object> save(@RequestBody Collect collect) {
+        if (StrUtil.isBlank(collect.getId())) {
+            collect.setId(IdUtil.simpleUUID());
+        }
+        boolean b = collectService.saveOrUpdate(collect);
+        return b ? ResultVo.success(2000,"收藏成功") : ResultVo.fail(5000,"收藏失败");
     }
 
     @PostMapping("/page")
@@ -31,9 +38,22 @@ public class CollectController {
 
     }
 
-    @GetMapping("/del/{id}")
+    @GetMapping("/collected/{userId}/{relId}")
+    @ApiOperation("用户是否关注该内容")
+    public ResultVo<String> collected(@PathVariable String userId, @PathVariable String relId) {
+        Collect one = collectService.getOne(Wrappers.lambdaQuery(Collect.class)
+                .eq(Collect::getUserId, userId)
+                .eq(Collect::getRelId, relId)
+                .eq(Collect::getFlag, '0'));
+        if (ObjectUtil.isNotEmpty(one)) {
+            return ResultVo.success(2000, "已收藏", one.getId());
+        }
+        return ResultVo.success(2000, "已收藏", null);
+    }
+
+    @DeleteMapping("/del/{id}")
     @ApiOperation("删除用户收藏信息")
-    public ResultVo<Object> del(@PathVariable String id) {
+    public ResultVo<Object> del(@PathVariable @NotBlank(message = "参数异常") String id) {
         boolean remove = collectService.remove(Wrappers.lambdaQuery(Collect.class)
                 .eq(Collect::getId, id));
         return remove ? ResultVo.success(2000, "删除成功") : ResultVo.fail(5000, "删除失败");
