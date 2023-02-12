563775a27aeab739461cfd0fd3e3de62cbf02ef2
yanfuyou <1839224665@qq.com>
2023-02-12 14:53:05 +0800

    拦截

--- src/main/java/com/fuyou/community/sys/config/MvcConfig.java
+++ src/main/java/com/fuyou/community/sys/config/MvcConfig.java
@@ -6,13 +6,17 @@ import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import com.fuyou.community.exception.ServiceException;
 import com.fuyou.community.sys.auth.AuthUtil;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * @Author yanfuyou
@@ -20,7 +24,13 @@ import java.io.File;
  * @Date 下午9:11 2022/9/8
  */
 @Configuration
+@ConfigurationProperties(prefix = "not-match-path")
+@PropertySource(value = "classpath:application.yml")
+@Data
 public class MvcConfig implements WebMvcConfigurer {
+
+    private final List<String> pathList;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String rootPath = System.getProperty("user.dir");
@@ -36,15 +46,17 @@ public class MvcConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {//    使用sa-token拦截器
-//        try {
-//            registry.addInterceptor(new SaInterceptor(handle -> {
-//                        SaRouter.match("/**")
-//                                .notMatch("/user/**", "/upload/**", "/favicon.ico", "/v2/**", "/swagger-resources/**", "/webjars/**", "/doc.html", "/error/**", "/code/**", "/user/login", "/user/signup", "/image/**", "/file/**", "article/**")
-//                                .check(rule -> AuthUtil.authMethod());
-//                    }))
-//                    .addPathPatterns("/**");
-//        }catch (NotLoginException e){
-//            throw new ServiceException(401,"请登录");
-//        }
+        System.out.println("读取开放api：");
+        pathList.stream().forEach(System.out::println);
+        try {
+            registry.addInterceptor(new SaInterceptor(handle -> {
+                        SaRouter.match("/**")
+                                .notMatch(pathList)
+                                .check(rule -> AuthUtil.authMethod());
+                    }))
+                    .addPathPatterns("/**");
+        }catch (NotLoginException e){
+            throw new ServiceException(401,"请登录");
+        }
     }
 }
